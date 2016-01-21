e78fe87cde079df36d0fb468fa227496cb5c4eb0
Dan Armendariz <danallan@cs.harvard.edu>
2016-01-20 19:41:26 -0500

    add new jterm plugins to offline config

--- ide50-offline/files/client-workspace-cs50.js
+++ ide50-offline/files/client-workspace-cs50.js
@@ -1,8 +1,12 @@
 "use strict";
 
 module.exports = function(options) {
-    // Remove C runner
+    // Remove runners we don't want
     delete options.runners["C (simple)"];
+    delete options.runners["PHP (cli)"];
+    delete options.runners["PHP (built-in web server)"];
+    delete options.runners["Apache httpd (PHP, HTML)"];
+
     options.projectName = "ide50-offline";
 
     var config = require("./client-default")(options);
@@ -10,8 +14,11 @@ module.exports = function(options) {
     var includes = [
         "plugins/c9.ide.cs50.simple/simple50",
         "plugins/c9.ide.cs50.info/info50",
-        "plugins/c9.ide.cs50.themes/themes50",
-        "plugins/c9.ide.cs50.previewer/previewer50"
+        "plugins/c9.ide.cs50.previewer/previewer50",
+        {
+            packagePath: "plugins/c9.ide.cs50.themes/themes50",
+            staticPrefix: options.staticPrefix + "/plugins/c9.ide.cs50.themes"
+        }
     ];
 
     var excludes = {
